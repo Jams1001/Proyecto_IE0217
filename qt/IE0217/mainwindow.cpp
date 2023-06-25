9dc1392a3d04a228f7d68b6fdba411460311d3f9
Jams1001 <adantumora90@gmail.com>
2023-06-24 20:43:03 -0600

    funciones modulares
    Signed-off-by: Jams1001 <adantumora90@gmail.com>

--- qt/IE0217/mainwindow.cpp
+++ qt/IE0217/mainwindow.cpp
@@ -6,7 +6,6 @@
 #include <QInputDialog>
 #include <QLineEdit>
 
-
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -15,25 +14,31 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
     layout_Semesters = new QVBoxLayout;
     ui->scrollAreaWidget_Semesters->setLayout(layout_Semesters);
-    QVBoxLayout *layout2 = new QVBoxLayout;
-    ui->Semesters->setLayout(layout2);
+    layout_Teachers = new QVBoxLayout;
+    ui->scrollAreaWidget_Teachers->setLayout(layout_Teachers);
+    layout_ExternalCourses = new QVBoxLayout;
+    ui->scrollAreaWidget_ExternalCourses->setLayout(layout_ExternalCourses);
+    layout_Cycles = new QVBoxLayout;
+    ui->scrollAreaWidget_Cycles->setLayout(layout_Cycles);
+    layout_Courses = new QVBoxLayout;
+    ui->scrollAreaWidget_Courses->setLayout(layout_Courses);
+    layout_Classrooms = new QVBoxLayout;
+    ui->scrollAreaWidget_Classrooms->setLayout(layout_Classrooms);
 
     // Conectar botones para cada pestaña
-    connectButtonsAndLayouts("Semesters");
-    connectButtonsAndLayouts("Teachers");
-    connectButtonsAndLayouts("ExternalCourses");
-    connectButtonsAndLayouts("Cycles");
-    connectButtonsAndLayouts("Courses");
-    connectButtonsAndLayouts("Classrooms");
+    connectButtons("Semesters");
+    connectButtons("Teachers");
+    connectButtons("ExternalCourses");
+    connectButtons("Cycles");
+    connectButtons("Courses");
+    connectButtons("Classrooms");
 
     // Ocultar botones al inicio
     hideButtons();
 
 }
 
-
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
@@ -50,10 +55,7 @@ void MainWindow::on_pushButton_HomeCy_clicked(){ui->stackedWidget->setCurrentInd
 void MainWindow::on_pushButton_HomeCo_clicked(){ui->stackedWidget->setCurrentIndex(0);}
 void MainWindow::on_pushButton_HomeCl_clicked(){ui->stackedWidget->setCurrentIndex(0);}
 
-
-
-void MainWindow::connectButtonsAndLayouts(const QString &tabName)
-{
+void MainWindow::connectButtons(const QString &tabName) {
 
     QPushButton *buttonAdd = findChild<QPushButton*>(QString("pushButtonAdd_%1").arg(tabName));
     QPushButton *buttonSelect = findChild<QPushButton*>(QString("pushButtonSelect_%1").arg(tabName));
@@ -68,8 +70,7 @@ void MainWindow::connectButtonsAndLayouts(const QString &tabName)
     if (buttonDuplicate) connect(buttonDuplicate, &QPushButton::clicked, this, &MainWindow::duplicateSelectedButtons);
 }
 
-void MainWindow::hideButtons()
-{
+void MainWindow::hideButtons() {
     const QStringList tabNames = {"Semesters", "Teachers", "ExternalCourses", "Cycles", "Courses", "Classrooms"};
 
     for (const QString &tabName : tabNames) {
@@ -83,17 +84,18 @@ void MainWindow::hideButtons()
     }
 }
 
-
-void MainWindow::addNewButton()
-{
+void MainWindow::addNewButton() {
+    QVBoxLayout *currentLayout = getCurrentLayout(); // Obtiene el layout actual
+    if (!currentLayout) return; // Si no hay un layout actual, sale de la función
+    
     dialog_wb dialog(this); // Crea una instancia de tu cuadro de diálogo personalizado
     if (dialog.exec() == QDialog::Accepted) { // Muestra el cuadro de diálogo y verifica si el usuario hizo clic en "Agregar"
         QString buttonName = dialog.getButtonName(); // Obtiene el nombre del botón ingresado por el usuario
 
         // Verifica si ya existe un botón con ese nombre
         bool exists = false;
-        for (int i = 0; i < layout_Semesters->count(); ++i) {
-            QPushButton *button = qobject_cast<QPushButton*>(layout_Semesters->itemAt(i)->widget());
+        for (int i = 0; i < currentLayout->count(); ++i) {
+            QPushButton *button = qobject_cast<QPushButton*>(currentLayout->itemAt(i)->widget());
             if (button && button->text() == buttonName) {
                 exists = true;
                 break;
@@ -109,7 +111,7 @@ void MainWindow::addNewButton()
             newButton->setText(buttonName);
             connect(newButton, &QPushButton::clicked, this, &MainWindow::buttonClicked);
             newButton->installEventFilter(this); // Instala un filtro de eventos para detectar el doble clic
-            layout_Semesters->addWidget(newButton);
+            currentLayout->addWidget(newButton);
         }
     }
 }
@@ -118,41 +120,74 @@ void MainWindow::addNewButton()
 void MainWindow::enterSelectionMode()
 {
     isSelectionMode = true;
-    ui->pushButtonAdd_Semesters->hide();
-    ui->pushButtonSelect_Semesters->hide();
-    ui->pushButtonUnselect_Semesters->show();
-    ui->pushButtonDuplicate_Semesters->show();
-    ui->pushButtonRemove_Semesters->show();
+
+    QVBoxLayout *currentLayout = getCurrentLayout(); // Obtiene el layout actual
+    if (!currentLayout) return; // Si no hay un layout actual, sale de la función
+
+    QString tabName = getTabNameFromLayout(currentLayout);
+
+    // Buscar y acceder a los botones de la pestaña actual
+    QPushButton *buttonAdd = findChild<QPushButton*>(QString("pushButtonAdd_%1").arg(tabName));
+    QPushButton *buttonSelect = findChild<QPushButton*>(QString("pushButtonSelect_%1").arg(tabName));
+    QPushButton *buttonUnselect = findChild<QPushButton*>(QString("pushButtonUnselect_%1").arg(tabName));
+    QPushButton *buttonRemove = findChild<QPushButton*>(QString("pushButtonRemove_%1").arg(tabName));
+    QPushButton *buttonDuplicate = findChild<QPushButton*>(QString("pushButtonDuplicate_%1").arg(tabName));
+
+    // Ocultar/Mostrar botones
+    if (buttonAdd) buttonAdd->hide();
+    if (buttonSelect) buttonSelect->hide();
+    if (buttonUnselect) buttonUnselect->show();
+    if (buttonRemove) buttonRemove->show();
+    if (buttonDuplicate) buttonDuplicate->show();
 }
 
 void MainWindow::exitSelectionMode()
 {
     isSelectionMode = false;
-    ui->pushButtonAdd_Semesters->show();
-    ui->pushButtonSelect_Semesters->show();
-    ui->pushButtonUnselect_Semesters->hide();
-    ui->pushButtonDuplicate_Semesters->hide();
-    ui->pushButtonRemove_Semesters->hide();
+
+    QVBoxLayout *currentLayout = getCurrentLayout(); // Obtiene el layout actual
+    if (!currentLayout) return; // Si no hay un layout actual, sale de la función
+
+    QString tabName = getTabNameFromLayout(currentLayout);
+
+    // Buscar y acceder a los botones de la pestaña actual
+    QPushButton *buttonAdd = findChild<QPushButton*>(QString("pushButtonAdd_%1").arg(tabName));
+    QPushButton *buttonSelect = findChild<QPushButton*>(QString("pushButtonSelect_%1").arg(tabName));
+    QPushButton *buttonUnselect = findChild<QPushButton*>(QString("pushButtonUnselect_%1").arg(tabName));
+    QPushButton *buttonRemove = findChild<QPushButton*>(QString("pushButtonRemove_%1").arg(tabName));
+    QPushButton *buttonDuplicate = findChild<QPushButton*>(QString("pushButtonDuplicate_%1").arg(tabName));
+
+    // Mostrar/Ocultar botones
+    if (buttonAdd) buttonAdd->show();
+    if (buttonSelect) buttonSelect->show();
+    if (buttonUnselect) buttonUnselect->hide();
+    if (buttonRemove) buttonRemove->hide();
+    if (buttonDuplicate) buttonDuplicate->hide();
 }
+
 
 void MainWindow::removeSelectedButtons()
 {
+    QVBoxLayout *currentLayout = getCurrentLayout(); // Obtiene el layout actual
+    if (!currentLayout) return; // Si no hay un layout actual, sale de la función
+
     // Itera en reversa para no alterar el índice de los elementos restantes al eliminar
-    for (int i = layout_Semesters->count() - 1; i >= 0; --i) {
-        QPushButton *button = qobject_cast<QPushButton*>(layout_Semesters->itemAt(i)->widget());
+    for (int i = currentLayout->count() - 1; i >= 0; --i) {
+        QPushButton *button = qobject_cast<QPushButton*>(currentLayout->itemAt(i)->widget());
         if (button && button->property("isSelected").toBool()) {
             // Remueve el botón del layout y lo elimina
-            layout_Semesters->removeWidget(button);
+            currentLayout->removeWidget(button);
             delete button;
         }
     }
 }
 
-
-void MainWindow::duplicateSelectedButtons()
-{
-    for (int i = 0; i < layout_Semesters->count(); ++i) {
-        QPushButton *button = qobject_cast<QPushButton*>(layout_Semesters->itemAt(i)->widget());
+void MainWindow::duplicateSelectedButtons() {
+    QVBoxLayout *currentLayout = getCurrentLayout(); // Obtiene el layout actual
+    if (!currentLayout) return; // Si no hay un layout actual, sale de la función
+    
+    for (int i = 0; i < currentLayout->count(); ++i) {
+        QPushButton *button = qobject_cast<QPushButton*>(currentLayout->itemAt(i)->widget());
         if (button && button->property("isSelected").toBool()) {
             QString originalName = button->text();
             QString newName = originalName;
@@ -162,8 +197,8 @@ void MainWindow::duplicateSelectedButtons()
             bool nameExists;
             do {
                 nameExists = false;
-                for (int j = 0; j < layout_Semesters->count(); ++j) {
-                    QPushButton *otherButton = qobject_cast<QPushButton*>(layout_Semesters->itemAt(j)->widget());
+                for (int j = 0; j < currentLayout->count(); ++j) {
+                    QPushButton *otherButton = qobject_cast<QPushButton*>(currentLayout->itemAt(j)->widget());
                     if (otherButton && otherButton->text() == newName) {
                         nameExists = true;
                         newName = QString("%1 (%2)").arg(originalName).arg(duplicateCount++);
@@ -179,15 +214,13 @@ void MainWindow::duplicateSelectedButtons()
             connect(newButton, &QPushButton::clicked, this, &MainWindow::buttonClicked);
             newButton->installEventFilter(this);
 
-            layout_Semesters->addWidget(newButton);
+            currentLayout->addWidget(newButton);
         }
     }
 }
 
 
-
-void MainWindow::buttonClicked()
-{
+void MainWindow::buttonClicked() {
     if (!isSelectionMode) return; // Si no estamos en modo de selección, no hacemos nada
 
     QPushButton *button = qobject_cast<QPushButton*>(sender());
@@ -206,8 +239,7 @@ void MainWindow::buttonClicked()
 }
 
 
-bool MainWindow::eventFilter(QObject *watched, QEvent *event)
-{
+bool MainWindow::eventFilter(QObject *watched, QEvent *event) {
     if (!isSelectionMode && event->type() == QEvent::MouseButtonDblClick) {
         QPushButton *button = qobject_cast<QPushButton*>(watched);
         if (button) {
@@ -223,4 +255,44 @@ bool MainWindow::eventFilter(QObject *watched, QEvent *event)
         }
     }
     return QMainWindow::eventFilter(watched, event); // Llama a la implementación base
+}
+
+
+QVBoxLayout* MainWindow::getCurrentLayout() {
+    int currentIndex = ui->stackedWidget->currentIndex();
+    
+    switch(currentIndex) {
+        case 1:
+            return layout_Semesters;
+        case 2:
+            return layout_Teachers;
+        case 3:
+            return layout_ExternalCourses;
+        case 4:
+            return layout_Cycles;
+        case 5:
+            return layout_Courses;
+        case 6:
+            return layout_Classrooms;
+        default:
+            return nullptr;
+    }
+}
+
+QString MainWindow::getTabNameFromLayout(QVBoxLayout *layout) {
+    if (layout == layout_Semesters) {
+        return "Semesters";
+    } else if (layout == layout_Teachers) {
+        return "Teachers";
+    } else if (layout == layout_ExternalCourses) {
+        return "ExternalCourses";
+    } else if (layout == layout_Cycles) {
+        return "Cycles";
+    } else if (layout == layout_Courses) {
+        return "Courses";
+    } else if (layout == layout_Classrooms) {
+        return "Classrooms";
+    } else {
+        return QString();
+    }
 }
